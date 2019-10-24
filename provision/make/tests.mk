c3f5defc24f12a7af0d31d83a46ed198d72a8644
@slovacus <slovacus@gmail.com>
2019-10-24 11:55:10 -0500

    (#7) refactor: docker version python

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -11,12 +11,10 @@ test: clean
 	@if [ -z "${run}" ]; then \
 		make test.help;\
 	fi
-	@if [ -n "${run}" ]; then \
 
 test.all: clean
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
-	$(DOCKER_COMPOSE_TEST) run --rm $(SERVICE_APP) bash -c \
-		"$(PIPENV_RUN) pytest"
+	$(docker-test-run) bash -c "$(PIPENV_RUN) pytest"
 
 test.lint: clean
-	$(PIPENV_RUN) pre-commit run --all-files --verbose
+	$(docker-test-run) bash -c "$(PIPENV_RUN) pre-commit run --all-files --verbose"
